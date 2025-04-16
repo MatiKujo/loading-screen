f29b607af00acd18953cd8f1ebfa395df6107fc3
Mateusz Kozdra <matikozdra2115@wp.pl>
2025-04-16 10:50:53 +0200

    music box
    working music box

--- One Piece/songs.js
+++ One Piece/songs.js
@@ -8,10 +8,10 @@ const songs = [
 let currentSongIndex = 0;
 let isPlaying = false;
 let currentVolume = 100;
+let isPlayerOpen = false;
 const audio = new Audio(songs[currentSongIndex].url);
 audio.volume = currentVolume / 100;
 
-// Aktualizacja głośności i paska
 const rangeSlide = (value) => {
   const volume = value / 100;
   audio.volume = volume;
@@ -20,39 +20,30 @@ const rangeSlide = (value) => {
   document.querySelector('.range').value = value;
 };
 
-// Załaduj nowy utwór
 const loadSong = (index) => {
-  const songInfoContainer = document.querySelector('.song-info');
   const song = songs[index];
-
   const html = `
-    <div class="song-window">
-      <p>${song.title}</p>
-      <p>${song.author}</p>
-    </div>
+    <p>${song.title}</p>
+    <p>${song.author}</p>
   `;
 
-  songInfoContainer.innerHTML = html;
+  document.querySelector('.song-info').innerHTML = html;
+
   audio.src = song.url;
   isPlaying = false;
   updatePlayPauseIcon();
 };
 
-
-
-// Przejdź do następnego utworu
 const nextSong = () => {
   currentSongIndex = (currentSongIndex + 1) % songs.length;
   loadSong(currentSongIndex);
 };
 
-// Przejdź do poprzedniego utworu
 const prevSong = () => {
   currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
   loadSong(currentSongIndex);
 };
 
-// Odtwarzaj / pauzuj
 const toggleSongPause = () => {
   if (isPlaying) {
     audio.pause();
@@ -63,53 +54,40 @@ const toggleSongPause = () => {
   updatePlayPauseIcon();
 };
 
-// Aktualizacja ikony play/pause
 const updatePlayPauseIcon = () => {
   const icon = document.getElementById('playPauseIcon');
   icon.className = isPlaying ? "ri-pause-line" : "ri-play-line";
 };
 
-// Obsługa klawiszy
-document.addEventListener('keydown', (event) => {
-  if (event.code === 'Space') {
-    event.preventDefault();
-    toggleSongPause();
-  }
+// Rozwijanie music-box
+const musicBox = document.querySelector('.music-box');
 
-  if (event.code === 'ArrowRight') {
-    event.preventDefault();
-    nextSong();
-  }
+musicBox.addEventListener('click', () => {
+  isPlayerOpen = !isPlayerOpen;
+  musicBox.classList.toggle('expanded', isPlayerOpen);
 
-  if (event.code === 'ArrowLeft') {
-    event.preventDefault();
-    prevSong();
-  }
-
-  if (event.code === 'ArrowDown') {
-    event.preventDefault();
-    if (currentVolume > 0) {
-      currentVolume -= 1;
-      if (currentVolume < 0) currentVolume = 0;
-      rangeSlide(currentVolume);
-    }
-  }
-
-  if (event.code === 'ArrowUp') {
-    event.preventDefault();
-    if (currentVolume < 100) {
-      currentVolume += 1;
-      if (currentVolume > 100) currentVolume = 100;
-      rangeSlide(currentVolume);
-    }
+  if (isPlayerOpen) {
+    const html = `
+      <div class="song-info">
+        <p>${songs[currentSongIndex].title}</p>
+        <p>${songs[currentSongIndex].author}</p>
+      </div>
+      <input type="range" class="range" min="0" max="100" value="${currentVolume}" onchange="rangeSlide(this.value)" />
+      <span class="rangeValue">${currentVolume}%</span>
+      <div class="music-controls">
+        <button onclick="prevSong()">⏮</button>
+        <button onclick="toggleSongPause()"><i id="playPauseIcon" class="ri-play-line"></i></button>
+        <button onclick="nextSong()">⏭</button>
+      </div>
+    `;
+    musicBox.innerHTML = html;
+    updatePlayPauseIcon();
+  } else {
+    musicBox.innerHTML = `<i class="ri-spotify-line"></i><div class="song-info"></div>`;
   }
 });
 
-// Automatyczne przejście do następnej piosenki
-audio.addEventListener('ended', nextSong);
-
 window.onload = () => {
   loadSong(currentSongIndex);
-  rangeSlide(currentVolume); // opcjonalnie, jeśli chcesz też od razu ustawić suwak głośności
+  rangeSlide(currentVolume);
 };
-
